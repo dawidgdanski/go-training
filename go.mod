b7575098faf8dde0d41ab040292132b169fc46da
Dawid Gdański <davidgdanski@gmail.com>
2025-02-24 00:16:38 +0100

    Custom error struct

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module go-training
 go 1.23.0
 
 toolchain go1.23.2
+
+require github.com/google/uuid v1.6.0 // indirect
--- language/errors.go
+++ language/errors.go
@@ -5,10 +5,19 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"math/rand"
+
+	"github.com/google/uuid"
+)
+
+const (
+	ErrorFoo = Sentinel("foo error")
+	ErrorBar = Sentinel("bar error")
 )
 
 func init() {
 	openZipFile()
+	LoginAndGetData(uuid.New().String(), "pass123", "secret.txt")
 }
 
 func openZipFile() {
@@ -18,5 +27,67 @@ func openZipFile() {
 	if errors.Is(err, zip.ErrFormat) {
 		fmt.Println("Told you so")
 	}
+}
 
+type Sentinel string
+
+func (s Sentinel) Error() string {
+	return string(s)
+}
+
+type Status int
+
+const (
+	InvalidLogin Status = iota + 1
+	NotFound
+)
+
+type StatusErr struct {
+	Status  Status
+	Message string
+}
+
+func (se StatusErr) Error() string {
+	return se.Message
+}
+
+func LoginAndGetData(uid, password, file string) ([]byte, error) {
+	token, err := login(uid, password)
+	if err != nil {
+		return nil, StatusErr{
+			Status:  InvalidLogin,
+			Message: fmt.Sprintf("invalid credentials for user: %s", uid),
+		}
+	}
+	data, err := getData(token, file)
+	if err != nil {
+		return nil, StatusErr{
+			Status:  NotFound,
+			Message: fmt.Sprintf("Data not found for file %s: %s", uid, file),
+		}
+	}
+	fmt.Println(data)
+	return []byte(""), nil
+}
+
+func getData(token string, file string) (string, error) {
+	random := newRandom()
+	if random.Int31()%2 == 0 {
+		return fmt.Sprintf("Data %s", file), nil
+	} else {
+		return "", fmt.Errorf("could not find the data for file: %s", file)
+	}
+}
+
+func newRandom() *rand.Rand {
+	return rand.New(rand.NewSource(23))
+}
+
+func login(uid string, password string) (string, error) {
+	random := newRandom()
+	if random.Int31()%2 == 0 {
+		return uid, nil
+	} else {
+		return "", errors.New("Unsuccessful login")
+	}
 }
